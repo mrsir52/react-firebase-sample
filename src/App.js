e03e8c652f9648064eeb450a9f60306a94bd2a47
Manny <coachmannytw@gmail.com>
2019-03-19 13:23:51 -0600

    skeleton example, needs some refactoring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "./firebase";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hoursWorked: "",
+      username: "",
+      items: []
+    };
+    
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      title: this.state.hoursWorked,
+      user: this.state.username
+    }
+    itemsRef.push(item);
+    this.setState({
+      hoursWorked: '',
+      username: ''
+    });
+  }
+
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user
+        });
+      }
+      this.setState({
+        items: newState
+      });
+    });
+  }
+
+  removeItem(itemId) {
+    const itemRef = firebase.database().ref(`/items/${itemId}`);
+    itemRef.remove();
+  }
   render() {
+    console.log(this.state)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header>
+          <div className="wrapper">
+            <h1>Fun Food Friends</h1>
+          </div>
         </header>
+        <div className="container">
+          <section className="add-item">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                name="username"
+                placeholder="What's your name?"
+                onChange={this.handleChange}
+                value={this.state.username}
+              />
+              <input
+                type="text"
+                name="hoursWorked"
+                placeholder="How many hours did you work?"
+                onChange={this.handleChange}
+                value={this.state.hoursWorked}
+              />
+              <button>Add Item</button>
+            </form>
+          </section>
+          <section className="display-item">
+            <div className="wrapper">
+              <ul />
+            </div>
+          </section>
+        </div>
+        <section className='display-item'>
+              <div className="wrapper">
+                <ul>
+                  {this.state.items.map((item) => {
+                    return (
+                      <li key={item.id}>
+                        <h3>Hours worked: {item.title}</h3>
+                        <p>Worked by: {item.user}
+                          <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
+                        </p>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+          </section>
       </div>
     );
   }
